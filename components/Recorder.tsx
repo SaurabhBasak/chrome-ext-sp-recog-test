eb1d1cb3bdd17359549f18d0e4b74e36459f46ce
Saurabh Basak <saurabhbasak1@gmail.com>
2024-10-20 11:04:20 -0400

    Added speech recognition and voice activated recording

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,34 +1,75 @@
 "use client";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 function Recorder() {
     const [isRecording, setIsRecording] = useState(false);
-    const mediaRecorder = useRef<MediaRecorder | null>(null);
-    const chunks = useRef<Blob[]>([]);
+    const [isListening, setIsListening] = useState(false);
 
-    useEffect(() => {
-        navigator.mediaDevices
-            .getUserMedia({ audio: true, video: false })
-            .then((stream) => {
-                mediaRecorder.current = new MediaRecorder(stream);
-                mediaRecorder.current.ondataavailable = (e) => {
-                    chunks.current.push(e.data);
-                };
-                mediaRecorder.current.onstop = () => {
-                    const blob = new Blob(chunks.current, {
-                        type: "audio/ogg; codecs=opus",
-                    });
-                    const url = URL.createObjectURL(blob);
-                    console.log(url);
-                    const audio = new Audio(url);
-                    audio.play();
-                };
-            });
+    const startRecording = useCallback(async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+        });
+        const mediaRecorder = new MediaRecorder(stream);
 
-        return () => {
-            mediaRecorder.current = null;
+        const temporaryRecognition = new webkitSpeechRecognition();
+
+        temporaryRecognition.start();
+
+        mediaRecorder.start();
+        setIsRecording(true);
+        console.log("Recording started");
+
+        mediaRecorder.ondataavailable = function (event) {
+            const blob = new Blob([event.data], { type: "audio/wav" });
+            const url = URL.createObjectURL(blob);
+            console.log(url);
+        };
+
+        temporaryRecognition.onspeechend = function () {
+            console.log("Speech ended");
+            temporaryRecognition.stop();
+            mediaRecorder.stop();
+            mediaRecorder.onstop = function () {
+                console.log("Recording stopped");
+                setIsRecording(false);
+                setIsListening(false);
+            };
         };
     }, []);
+
+    const startListening = useCallback(() => {
+        const recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.lang = "en-US";
+        recognition.interimResults = false;
+        recognition.start();
+        setIsListening(true);
+        console.log("Recognition started");
+
+        recognition.onresult = function (event) {
+            const transcript = event.results[
+                event.results.length - 1
+            ][0].transcript
+                .trim()
+                .toLowerCase();
+            console.log(transcript);
+
+            if (transcript.includes("jarvis")) {
+                recognition.stop();
+                recognition.onend = async function () {
+                    await startRecording();
+                    console.log("jarvis detected. Listening for command...");
+                };
+            }
+        };
+    }, [startRecording]);
+
+    useEffect(() => {
+        console.log(isListening, isRecording);
+        if (!isListening && !isRecording) {
+            startListening();
+        }
+    }, [isListening, isRecording, startListening]);
 
     return (
         <div>
